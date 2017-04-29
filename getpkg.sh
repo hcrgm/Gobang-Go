3806a37b2937684bbd3dba5d792848ffef0d414f
hcrgm <hcrgm@qq.com>
2017-04-29 20:26:18 +0800

    Re-organize the project

--- getpkg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export GOPATH=`pwd`
-go get github.com/labstack/echo
-go get github.com/auth0/go-jwt-middleware
-go get github.com/gorilla/websocket
-go get github.com/bitly/go-simplejson
-go get github.com/kataras/go-sessions
--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-export GOPATH=`pwd`
-#echo "Getting packages"
-#./getpkg.sh
-echo "Installing"
-go build src/gobang.go
-echo "Build success"
